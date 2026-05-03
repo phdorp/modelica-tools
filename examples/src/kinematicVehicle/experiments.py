f53ae95d7637a8400b9fc0a2a5b9097529f4078f
phdorp <philip.dorpmueller@web.de>
2026-05-03 19:14:15 +0000

    feat: apply registry wrapper to example experiment

--- examples/src/kinematicVehicle/experiments.py
+++ examples/src/kinematicVehicle/experiments.py
@@ -1,14 +1,7 @@
-from hydra_zen import make_config, MISSING
-from kinematicVehicle.kinematicVehicle import run_default, store
+from kinematicVehicle.kinematicVehicle import registry, run_default
 
-experiment_store = store(group="experiment", package="_global_")
-
-experiment_store(
-    make_config(
-        bases=(run_default,),
-        model_name=MISSING,
-        session=MISSING,
-        hydra_defaults=["_self_", {"override /state_0": "front_position"}],
-    ),
+registry.register_experiment(
     name="front_position",
+    base_run_config=run_default,
+    selections={"session.parameters.state_0": "front_position"},
 )
